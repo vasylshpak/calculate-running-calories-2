6e8209629cca7177114dcdf30e3e2a9e1f0f2aa4
Vasyl Shpak <vasyl.shpak4@gmail.com>
2019-11-18 20:34:26 +0100

    Updates

---
+++ precache-manifest.db4454c61ecafbeb3cc90215ab63ae9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ba77dea69f47811c9025157a8e11515",
+    "url": "/calculate-running-calories-2/index.html"
+  },
+  {
+    "revision": "e0a89b71fa9419290b5f",
+    "url": "/calculate-running-calories-2/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "71fde760fab1e705afb0",
+    "url": "/calculate-running-calories-2/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e0a89b71fa9419290b5f",
+    "url": "/calculate-running-calories-2/static/js/2.bc37db9d.chunk.js"
+  },
+  {
+    "revision": "71fde760fab1e705afb0",
+    "url": "/calculate-running-calories-2/static/js/main.31d1a8ab.chunk.js"
+  },
+  {
+    "revision": "d51fddf328ba83b710d6",
+    "url": "/calculate-running-calories-2/static/js/runtime-main.b5a81252.js"
+  }
+]);
